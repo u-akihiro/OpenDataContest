f6377f1f626646d9d13613f1f458b87fe45beea8
vagrant <vagrant@precise>
2014-01-18 08:13:49 +0000

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenDataContest::Application.config.secret_key_base = '673665a4204cb3b15fdd880331abe06a0622419130dcf87bb408cd0b1cf504f2c5c2c04b3cd6c95c34c25c262ba816480fab56e1c17cf8ee86825ddf6c17bdc8'
