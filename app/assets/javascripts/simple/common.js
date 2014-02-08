41b2192c5766f10d1a476f79be998b400849ef36
vagrant <vagrant@precise>
2014-02-08 09:04:16 +0000

    modify

--- app/assets/javascripts/simple/common.js
+++ app/assets/javascripts/simple/common.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
     var kyotoSimpleController = new KyotoSimpleController();
     
-    kyotoSimpleController.setView(new KyotoSimpleView());
+    //kyotoSimpleController.setView(new KyotoSimpleView());
+	if( $('#params').val() == "ugx_Cookshop" ) {
+		kyotoSimpleController.setView(new KyotoSimpleView());
+	} else if ( $('#params').val() == "ugx_Sightseeing" ) {
+		kyotoSimpleController.setView(new KyotoSightseeingView());
+	}
     kyotoSimpleController.load();
     kyotoSimpleController.moreEvent();
-    
+
 });
